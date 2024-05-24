76cf628dce0ef4518557ce6e3191d937c6016bf6
vincent-1014 <vincent.jeong@paust.io>
2024-05-25 03:39:45 +0900

    feat: Add Modal components for stack details page

---
+++ src/app/projects/stack/[stack_id]/AddInstanceModal.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import Button from '@/app/_components/Button';
+import DialogModal from '@/app/_components/Dialog';
+import Dropdown from '@/app/_components/Dropdown';
+
+// TODO: Change the instance zone items
+const INSTANCE_ZONE_ITEMS = [
+  { label: 'east-korea', value: 'east-korea' },
+  { label: 'east-asia', value: 'east-asia' },
+  { label: 'east-japan', value: 'east-japan' },
+  { label: 'east-us', value: 'east-us' },
+  { label: 'west-us', value: 'west-us' },
+];
+
+type Props = {
+  onAdded: () => void;
+  onCancel: () => void;
+};
+
+const AddInstanceModal = ({ onAdded, onCancel }: Props) => {
+  const [instanceZone, setInstanceZone] = useState<string | null>(null);
+  const [instanceName, setInstanceName] = useState<string>('');
+  const [showError, setShowError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleAddInstance = async () => {
+    // TODO: Implement add instance feature
+  };
+
+  return (
+    <div className="flex flex-col gap-y-6">
+      <div>
+        <span className="font-semibold text-xl">인스턴스 추가하기</span>
+      </div>
+      <div className="flex flex-col gap-y-3">
+        <div className="w-full">
+          <Dropdown
+            className="max-w-40"
+            placeholder="Zone"
+            items={INSTANCE_ZONE_ITEMS}
+            selectedValue={instanceZone}
+            onSelected={(item) => setInstanceZone(item.value)}
+          />
+        </div>
+        <div className="w-full">
+          <input
+            type="text"
+            placeholder="Stack-Instance-SK"
+            value={instanceName}
+            onChange={(e) => setInstanceName(e.target.value)}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div className="flex justify-end items-center gap-x-6">
+        <Button color="secondary" onClick={onCancel}>
+          cancel
+        </Button>
+        <Button color="primary" onClick={handleAddInstance}>
+          Add
+        </Button>
+      </div>
+      <DialogModal
+        open={showError}
+        setOpen={setShowError}
+        description={errorMessage}
+        type={'alert'}
+      />
+    </div>
+  );
+};
+
+export default AddInstanceModal;
--- src/app/projects/stack/[stack_id]/StackInfo.tsx
+++ src/app/projects/stack/[stack_id]/StackInfo.tsx
@@ -1,10 +1,13 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { trpc } from '@/app/_trpc/client';
 import { FigmaIcon, ServicePlanIcon } from '@/app/_components/Icons';
 import { Stack } from '@/models/stack';
 import Button from '@/app/_components/Button';
+import Modal from '@/app/_components/Modal';
+import AddReferenceModal from './AddReferenceModal';
+import AddInstanceModal from './AddInstanceModal';
 
 type Props = {
   stack: Stack;
@@ -13,140 +16,182 @@ type Props = {
 export default function StackInfo({ stack }: Props) {
   const { data: { instances, after } = {} } =
     trpc.stack.instances.list.useQuery({ stackId: stack.id });
+  const [showAddInstanceDialog, setShowAddInstanceDialog] =
+    useState<boolean>(false);
+  const [showAddReferenceDialog, setShowAddReferenceDialog] =
+    useState<boolean>(false);
 
-  const handleCreateInstance = () => {
-    // TODO: Implement create instance feature
+  const onInstanceAdded = () => {
+    // TODO: Implement on instance added
   };
 
   const handleEditDescription = () => {
     // TODO: Implement edit description feature
   };
 
-  const handleAddReference = () => {
-    // TODO: Implement add reference feature
+  const onReferenceAdded = () => {
+    // TODO: Implement on reference added
   };
 
   return (
-    <div className="mt-5 flex flex-col gap-x-6 gap-y-12">
-      <div>
-        <div className="flex items-center gap-x-4">
-          <label className="block text-base font-semibold leading-6 text-gray-900">
-            Description
-          </label>
-          <Button onClick={handleEditDescription}>Edit</Button>
-        </div>
-        <div className="mt-4">
-          <textarea
-            rows={5}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            defaultValue={stack?.description}
+    <>
+      <Modal
+        open={showAddReferenceDialog}
+        setOpen={setShowAddReferenceDialog}
+        contents={
+          <AddReferenceModal
+            onCancel={() => {
+              setShowAddReferenceDialog(false);
+            }}
+            onAdded={onReferenceAdded}
           />
-        </div>
-      </div>
-
-      <div>
-        <div className="flex items-center gap-x-4">
-          <label className="block text-base font-semibold leading-6 text-gray-900">
-            Reference
-          </label>
-          <Button onClick={() => {}}>Add</Button>
-        </div>
-        <div className="flex flex-col mt-4 gap-y-4">
-          {/* TODO: Implement get reference list feature */}
-          <div className="flex gap-x-6">
-            <FigmaIcon />
-            <span className="font-normal text-sm">와이어프레임 컨셉</span>
-            <button className="font-normal text-sm text-indigo-500 hover:cursor-pointer">
-              Edit
-            </button>
+        }
+      />
+      <Modal
+        open={showAddInstanceDialog}
+        setOpen={setShowAddInstanceDialog}
+        contents={
+          <AddInstanceModal
+            onCancel={() => {
+              setShowAddInstanceDialog(false);
+            }}
+            onAdded={onInstanceAdded}
+          />
+        }
+      />
+      <div className="mt-5 flex flex-col gap-x-6 gap-y-12">
+        <div>
+          <div className="flex items-center gap-x-4">
+            <label className="block text-base font-semibold leading-6 text-gray-900">
+              Description
+            </label>
+            <Button onClick={handleEditDescription}>Edit</Button>
           </div>
-          <div className="flex gap-x-6">
-            <ServicePlanIcon />
-            <span className="font-normal text-sm">서비스 기획서</span>
-            <button className="font-normal text-sm text-indigo-500 hover:cursor-pointer">
-              Edit
-            </button>
+          <div className="mt-4">
+            <textarea
+              rows={5}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              defaultValue={stack?.description}
+            />
           </div>
         </div>
-      </div>
 
-      <div>
-        <div className="flex items-center justify-between">
-          <label className="block text-base font-semibold leading-6 text-gray-900">
-            Instance List
-          </label>
-          <Button onClick={() => {}}>Create</Button>
+        <div>
+          <div className="flex items-center gap-x-4">
+            <label className="block text-base font-semibold leading-6 text-gray-900">
+              Reference
+            </label>
+            <Button
+              onClick={() => {
+                setShowAddReferenceDialog(true);
+              }}
+            >
+              Add
+            </Button>
+          </div>
+          <div className="flex flex-col mt-4 gap-y-4">
+            {/* TODO: Implement get reference list feature */}
+            <div className="flex gap-x-6">
+              <FigmaIcon />
+              <span className="font-normal text-sm">와이어프레임 컨셉</span>
+              <button className="font-normal text-sm text-indigo-500 hover:cursor-pointer">
+                Edit
+              </button>
+            </div>
+            <div className="flex gap-x-6">
+              <ServicePlanIcon />
+              <span className="font-normal text-sm">서비스 기획서</span>
+              <button className="font-normal text-sm text-indigo-500 hover:cursor-pointer">
+                Edit
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="mt-4 px-4 sm:px-6 lg:px-8">
-          <div className="flow-root">
-            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="inline-block min-w-full py-2 align-middle">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                      >
-                        Instance
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Amount
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        spec
-                      </th>
-                      <th
-                        scope="col"
-                        className="relative py-3.5 pl-3 pr-4 sm:pr-0"
-                      >
-                        region
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200">
-                    {/* TODO: Implement get Instance list feature */}
-                    {instances &&
-                      instances.length > 0 &&
-                      instances?.map((instance) => (
-                        <tr key={`instance-${instance.id}`}>
-                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                            {instance.id}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            1
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            {instance.state}
-                          </td>
-                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                            {instance.zone}
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label className="block text-base font-semibold leading-6 text-gray-900">
+              Instance List
+            </label>
+            <Button
+              onClick={() => {
+                setShowAddInstanceDialog(true);
+              }}
+            >
+              Create
+            </Button>
+          </div>
+          <div className="mt-4 px-4 sm:px-6 lg:px-8">
+            <div className="flow-root">
+              <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 align-middle">
+                  <table className="min-w-full divide-y divide-gray-300">
+                    <thead>
+                      <tr>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                        >
+                          Instance
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          Amount
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          spec
+                        </th>
+                        <th
+                          scope="col"
+                          className="relative py-3.5 pl-3 pr-4 sm:pr-0"
+                        >
+                          region
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200">
+                      {/* TODO: Implement get Instance list feature */}
+                      {instances &&
+                        instances.length > 0 &&
+                        instances?.map((instance) => (
+                          <tr key={`instance-${instance.id}`}>
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                              {instance.id}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              1
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {instance.state}
+                            </td>
+                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                              {instance.zone}
+                            </td>
+                          </tr>
+                        ))}
+                      {instances && instances.length === 0 && (
+                        <tr>
+                          <td
+                            colSpan={4}
+                            className="whitespace-nowrap text-center py-10 text-sm font-medium text-gray-900 sm:pl-0 "
+                          >
+                            No instances
                           </td>
                         </tr>
-                      ))}
-                    {instances && instances.length === 0 && (
-                      <tr>
-                        <td
-                          colSpan={4}
-                          className="whitespace-nowrap text-center py-10 text-sm font-medium text-gray-900 sm:pl-0 "
-                        >
-                          No instances
-                        </td>
-                      </tr>
-                    )}
-                  </tbody>
-                </table>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
