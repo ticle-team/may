b7926cb14f37304f17b5d0f56682bdf2bddd3868
1dennispark <dennis.park@paust.io>
2024-06-04 14:26:30 +0900

    fix: add button color supported and fix to make secondary color button is followed color-preset

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 
 type Props = React.PropsWithChildren<{
-  color?: 'primary' | 'secondary';
+  color?: 'primary' | 'secondary' | 'success';
   size?: 'xs' | 'sm' | 'base' | 'lg' | 'xl';
   disabled?: boolean;
   onClick?: () => void | Promise<void>;
@@ -15,17 +15,19 @@ export default function Button({
   disabled = false,
 }: Props) {
   const buttonClass = classNames({
-    'flex focus:outline-none focus:ring-2 focus:ring-opacity-50': true,
-    'text-white bg-primary-700 hover:bg-primary-600 focus:ring-primary-500':
+    'flex focus:outline-none focus:ring-0': true,
+    'text-white bg-primary-700 hover:bg-primary-600 focus:bg-primary-800 ring-primary-700':
       color === 'primary' && !disabled,
-    'text-gray-900 bg-white hover:bg-gray-50 focus:ring-primary-500 ring-1 ring-inset ring-gray-300':
+    'text-white bg-secondary-700 hover:bg-secondary-600 focus:bg-secondary-800 ring-secondary-700':
       color === 'secondary' && !disabled,
+    'text-white bg-success-700 hover:bg-success-600 focus:bg-success-800 ring-success-700':
+      color === 'success' && !disabled,
     'cursor-not-allowed bg-gray-300 text-gray-500': disabled,
     'font-semibold text-xs py-1 px-2 rounded': size === 'xs',
     'font-semibold text-sm py-1 px-2 rounded': size === 'sm',
-    'font-semibold text-sm py-1.5 px-2.5 rounded-md': size === 'base',
-    'font-semibold text-sm py-2 px-3 rounded-md': size === 'lg',
-    'font-semibold text-sm py-2.5 px-3.5 rounded-md': size === 'xl',
+    'font-semibold text-base py-1.5 px-2.5 rounded-md': size === 'base',
+    'font-semibold text-lg py-2 px-3 rounded-lg': size === 'lg',
+    'font-semibold text-xl py-2.5 px-3.5 rounded-lg': size === 'xl',
   });
 
   return (
