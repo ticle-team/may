234a2e8f582fac5f0f52c7a173b9a4dd7254f7d6
1dennispark <dennis.park@paust.io>
2024-06-10 23:37:20 +0900

    fix: dropdown component supports to enable scroll when its item list is longer

--- src/app/_components/Dropdown.tsx
+++ src/app/_components/Dropdown.tsx
@@ -49,7 +49,7 @@ export default function Dropdown({
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute w-full right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute w-full right-0 z-10 mt-2 max-h-fit overflow-y-auto origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="py-1">
               {items.map((item, index) => (
                 <Menu.Item key={`dropdown_item_${index}`}>
