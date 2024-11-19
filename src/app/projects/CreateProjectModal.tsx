1c1bbcc5116750d67049e6311cfbb49c20a354ac
1dennispark <dennis.park@paust.io>
2024-11-19 21:01:15 +0900

    fix: adding sentence in project name input field

--- src/app/projects/CreateProjectModal.tsx
+++ src/app/projects/CreateProjectModal.tsx
@@ -32,7 +32,7 @@ const CreateProjectModal = ({ onCreate, onCancel }: Props) => {
         <div className="w-full">
           <input
             type="text"
-            placeholder="name"
+            placeholder="name (without blank spaces)"
             value={projectName}
             onChange={(e) => setProjectName(e.target.value)}
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
